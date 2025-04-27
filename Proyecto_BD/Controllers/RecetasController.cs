a49c55fb65551a192ddeb3105b83b4c6c022c006
victo <victo@DESKTOP-1LKE3D3>
2025-04-27 01:26:32 -0600

    sa

---
+++ Proyecto_BD/Controllers/RecetasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_BD.Models;
+
+namespace Proyecto_BD.Controllers
+{
+    public class RecetasController : Controller
+    {
+        private readonly ContextoBaseDatos _context;
+
+        public RecetasController(ContextoBaseDatos context)
+        {
+            _context = context;
+        }
+
+        // GET: Recetas
+        public async Task<IActionResult> Index()
+        {
+            var contextoBaseDatos = _context.Receta.Include(r => r.Cita);
+            return View(await contextoBaseDatos.ToListAsync());
+        }
+
+        // GET: Recetas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var receta = await _context.Receta
+                .Include(r => r.Cita)
+                .FirstOrDefaultAsync(m => m.ID_Receta == id);
+            if (receta == null)
+            {
+                return NotFound();
+            }
+
+            return View(receta);
+        }
+
+        // GET: Recetas/Create
+        public IActionResult Create()
+        {
+            ViewData["ID_Cita"] = new SelectList(_context.Cita, "ID_Cita", "ID_Cita");
+            return View();
+        }
+
+        // POST: Recetas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID_Receta,ID_Cita,Diagnostico")] Receta receta)
+        {
+            if (receta.Diagnostico != "")
+            {
+                _context.Add(receta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ID_Cita"] = new SelectList(_context.Cita, "ID_Cita", "ID_Cita", receta.ID_Cita);
+            return View(receta);
+        }
+
+        // GET: Recetas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var receta = await _context.Receta.FindAsync(id);
+            if (receta == null)
+            {
+                return NotFound();
+            }
+            ViewData["ID_Cita"] = new SelectList(_context.Cita, "ID_Cita", "ID_Cita", receta.ID_Cita);
+            return View(receta);
+        }
+
+        // POST: Recetas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID_Receta,ID_Cita,Diagnostico")] Receta receta)
+        {
+            if (id != receta.ID_Receta)
+            {
+                return NotFound();
+            }
+
+            if (receta.Diagnostico != "")
+            {
+                try
+                {
+                    _context.Update(receta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecetaExists(receta.ID_Receta))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ID_Cita"] = new SelectList(_context.Cita, "ID_Cita", "ID_Cita", receta.ID_Cita);
+            return View(receta);
+        }
+
+        // GET: Recetas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var receta = await _context.Receta
+                .Include(r => r.Cita)
+                .FirstOrDefaultAsync(m => m.ID_Receta == id);
+            if (receta == null)
+            {
+                return NotFound();
+            }
+
+            return View(receta);
+        }
+
+        // POST: Recetas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var receta = await _context.Receta.FindAsync(id);
+            if (receta != null)
+            {
+                _context.Receta.Remove(receta);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecetaExists(int id)
+        {
+            return _context.Receta.Any(e => e.ID_Receta == id);
+        }
+    }
+}
